b25985bc2eb451489eb753faf1d3ed9304db8d77
Jose Sanchez <jparkers0012@gmail.com>
2015-06-01 11:39:20 -0400

    implement search

--- models/address_book.rb
+++ models/address_book.rb
@@ -39,4 +39,33 @@ class AddressBook
     end
     return csv.count
   end
+
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # if entries[mid].name > name
+      #   upper = mid - 1
+      # elsif entries[mid].name < name
+      #   lower = mid + 1
+      # else
+      #   return entries[mid]
+      # end
+          
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+    return nil
+  end
 end
